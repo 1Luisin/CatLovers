6768965532a3ef0f6694aa3cfd64b6f8b2729062
Luis Guilherme <guilherme32698@gmail.com>
2026-06-24 17:24:25 -0300

    Remove fallback obrigatorio do google services

--- app.config.js
+++ app.config.js
@@ -1,10 +1,1 @@
-module.exports = ({ config }) => ({
-  ...config,
-  android: {
-    ...config.android,
-    googleServicesFile:
-      process.env.GOOGLE_SERVICES_JSON ??
-      config.android?.googleServicesFile ??
-      "./google-services.json",
-  },
-});
+module.exports = ({ config }) => config;
